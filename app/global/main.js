e84b5f1b7f65533e20f60e1bb5fb2b3f108757cc
BrunoCartier <bruno.cartier@activkonnect.com>
2015-03-26 18:45:11 +0100

    Base servable Ionic app

---
+++ app/global/main.js
@@ -0,0 +1,13 @@
+/*jslint indent: 4, maxlen: 100 */
+/*global angular */
+
+(function (ng) {
+    'use strict';
+
+    var app = ng.module('akIonicTpl', [
+        'ionic',
+        'ngCordova'
+    ]);
+
+    ng.noop(app); // Of course, delete this line when doing something with app
+}(angular));
